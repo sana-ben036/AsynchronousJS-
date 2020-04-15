af52be18cd473507a652887e66af84de0b251029
SANA BEN <sana.bengannoune@gmail.com>
2020-04-16 00:52:04 +0100

    add list defi 2

--- Défi_2/app.js
+++ Défi_2/app.js
@@ -11,22 +11,23 @@ btn2.addEventListener('click', loadCustomers);
 
 
 // declaration de l'element ou on va afficher le resultat de la requete
-var result1 = document.getElementById('customer');
+var output1 = document.getElementById('customer');
 
-var result2 = document.getElementById('customers');
+var output2 = document.getElementById('customers');
 
 // fonction loadCustomer
 function loadCustomer() {
 
-
     let request1 = new XMLHttpRequest();
-
-
+    
     request1.onload = function () {
-        console.log(this);
 
+        console.log(this);
+        
         if(this.readyState == 4 && this.status == 200){
-            result1.innerHTML=this.response; 
+            var customer = this.response; 
+            output1.innerHTML= '<ul><li> id :' + customer.id +'</li><li> name :' + customer.name +'</li><li> company :' + customer.company +'</li><li> phone :' + customer.phone +'</li></ul>'
+
         }else if(this.readyState == 4 && this.status == 404){
             alert('Erreur 404');
         }
@@ -34,12 +35,13 @@ function loadCustomer() {
     };
 
     request1.open('GET', 'customer.json');
+    request1.responseType='json';
     request1.send();
 
 
    }
 
-   // fonction loadCustomers
+   // fonction loadCustomers 
 function loadCustomers() {
 
     let request2 = new XMLHttpRequest();
@@ -50,7 +52,18 @@ function loadCustomers() {
         console.log(this);
 
         if(this.readyState == 4 && this.status == 200){
-            result2.innerHTML=this.response; 
+            var data =this.response; 
+            var customer = data.customer;
+
+            for( var i=0; i<customer.length; i++){
+
+                output2.innerHTML = '<ul><li> id :' + customer[0].id +'</li><li> name :' + customer[0].name +'</li><li> company :' + customer[0].company +'</li><li> phone :' + customer[0].phone +'</li></ul>'
+
+               
+
+
+            }
+
         }else if(this.readyState == 4 && this.status == 404){
             alert('Erreur 404');
         }
@@ -58,6 +71,7 @@ function loadCustomers() {
     };
 
     request2.open('GET', 'customers.json');
+    request2.responseType='json';
     request2.send();
 
 
