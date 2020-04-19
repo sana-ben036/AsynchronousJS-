0d5eab386f9569e13e5d2a5ae801eea4857ee1d7
SANA BEN <sana.bengannoune@gmail.com>
2020-04-19 19:16:20 +0000

    modification interface

--- Defi_3/index.js
+++ Defi_3/index.js
@@ -1,35 +1,100 @@
-fetch("http://localhost:3000/books").then(
+
+var showlist= document.getElementById('show');
+var xhr= new XMLHttpRequest();
+showlist.addEventListener('click',showList);
+
+function showList(){
+
+xhr.onload= function(){
+    var data = this.response;
+    if(data.length>0){
+        var output= "";
+
+        // star for loop
+
+        data.forEach((u)=>{
+            output += `<tr>`;
+            output += `<td>${u.id}</td>`;
+            output += `<td>${u.author}</td>`;
+            output += `<td>${u.country}</td>`;
+            output += `<td>${u.imageLink}</td>`;
+            output += `<td>${u.language}</td>`;
+            output += `<td>${u.link}</td>`;
+            output += `<td>${u.pages}</td>`;
+            output += `<td>${u.title}</td>`;
+            output += `<td>${u.year}</td></tr>`;
+        })
+        // close for loop
+        document.getElementById("data").innerHTML= output;
+    }
+};
+xhr.open('get','http://localhost:3000/books');
+xhr.responseType='json';
+xhr.send();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*fetch("http://localhost:3000/books").then(
     res=>{
         res.json().then(
             data=>{
                 console.log(data);
                 if(data.length>0){
-                    var book= "";
+                    var output= "";
 
                     // star for loop
 
                     data.forEach((u)=>{
-                        book += `<tr>`;
-                        book += `<td>${u.id}</td>`;
-                        book += `<td>${u.author}</td>`;
-                        book += `<td>${u.country}</td>`;
-                        book += `<td>${u.imageLink}</td>`;
-                        book += `<td>${u.language}</td>`;
-                        book += `<td>${u.link}</td>`;
-                        book += `<td>${u.pages}</td>`;
-                        book += `<td>${u.title}</td>`;
-                        book += `<td>${u.year}</td></tr>`;
+                        output += `<tr>`;
+                        output += `<td>${u.id}</td>`;
+                        output += `<td>${u.author}</td>`;
+                        output += `<td>${u.country}</td>`;
+                        output += `<td>${u.imageLink}</td>`;
+                        output += `<td>${u.language}</td>`;
+                        output += `<td>${u.link}</td>`;
+                        output += `<td>${u.pages}</td>`;
+                        output += `<td>${u.title}</td>`;
+                        output += `<td>${u.year}</td></tr>`;
                     })
                     // close for loop
-                    document.getElementById("data").innerHTML= book;
+                    document.getElementById("data").innerHTML= output;
                 }
             }
         )
     }
 )
+*/
 
 
 
-
-var request = new XMLHttpRequest();
-request.open("POST", "http://localhost:3000/books");
+/*function sendData(){
+  
+    var XHR= new XMLHttpRequest();
+    var FD= new FormData(form);
+    XHR.addEventListener("load", function(event) {
+        alert(event.target.responseText);
+      });
+    XHR.addEventListener("error", function(event) {
+        alert('Oups! Quelque chose s\'est mal passé.');
+      });
+    XHR.open("POST", "http://localhost:3000/books");
+    XHR.send(FD);
+  
+  }
+  var form = document.getElementById("form");
+  form.addEventListener("submit", function (event) {
+      event.preventDefault();
+  
+      sendData();
+    });
+    */
