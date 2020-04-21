5a92588437b0df06fa386afd11662011f8b06fcf
SANA BEN <sana.bengannoune@gmail.com>
2020-04-21 00:49:56 +0000

    CRUD methode fetch

--- Defi_3/index.js
+++ Defi_3/index.js
@@ -1,6 +1,7 @@
-
+// 1- methode xmlhttprequest
 //----------------- afficher all books  (get)------------------------------------
 
+/*
 var show= document.getElementById('show');
 show.addEventListener('click',showList);
 
@@ -74,9 +75,6 @@ xhr2.send(jasonString)
 
     
 
-
-
-
 // -------------------update a book  (put)---------------------
 
 var update= document.getElementById('update');
@@ -120,139 +118,147 @@ function deletBook(){
 
 const jasonString = JSON.stringify(book);
 
-xhr4.open('DELETe','http://localhost:3000/books/1');
+xhr4.open('DELETE','http://localhost:3000/books/21');
 xhr4.setRequestHeader('Content-Type','application/json');
 xhr4.send()
 }
+*/
 
+// 2- methode fetch:
+//----------------- afficher all books  (get)------------------------------------
+/*
+let url = "http://localhost:3000/books"
+var show = document.getElementById('show')
+show.addEventListener('click',showList)
 
+function showList(){
+    fetch(url)
+    .then((res)=>{
+        return res.json()
+    })
+    .then((data)=>{
+        var output= "";
+        data.forEach((u)=>{
+            output += `<tr>`;
+            output += `<td>${u.id}</td>`;
+            output += `<td>${u.author}</td>`;
+            output += `<td>${u.country}</td>`;
+            output += `<td>${u.imageLink}</td>`;
+            output += `<td>${u.language}</td>`;
+            output += `<td>${u.link}</td>`;
+            output += `<td>${u.pages}</td>`;
+            output += `<td>${u.title}</td>`;
+            output += `<td>${u.year}</td></tr>`;
+        
+        })
 
+        document.getElementById("data").innerHTML= output;
 
+    })
+    .catch((err)=>{
+        console.error(err)
+    })
 
+}
 
+//----------------- add new book  (post)------------------------------------
 
+var add = document.getElementById('add');
+add.addEventListener('click',addBook);
 
+function addBook(){
+    fetch(url,{
+        method: 'POST',
+        headers:{
+           'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            "id" : 21,
+            "author" : "sana",
+            "country" : "morocco",
+            "imageLink" : "",
+            "language" : "arabic",
+            "link" : "",
+            "pages":"300", 
+            "title":"biography",
+            "year" :2020
+        }) 
+    })  
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+        document.getElementById("data").innerHTML= data;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function newBook(){
-
-    var FormData = {};
-
-    FormData["id"] = document.getElementById("id").value;
-    FormData["author"] = document.getElementById("author").value;
-    FormData["country"] = document.getElementById("country").value;
-    FormData["imageLink"] = document.getElementById("imageLink").value;
-    FormData["language"] = document.getElementById("language").value;
-    FormData["link"] = document.getElementById("link").value;
-    FormData["pages"] = document.getElementById("pages").value;
-    FormData["title"] = document.getElementById("title").value;
-    FormData["year"] = document.getElementById("year").value;
-
-    return newBook();
-
+    })
     
+    .catch((err)=>{
+        console.error(err)
+    })
+}
+
+//----------------- update a book (put)------------------------------------
+var update = document.getElementById('update');
+update.addEventListener('click',updateBook);
+
+
+function updateBook(){
+    let url = 'http://localhost:3000/books/21'
+    fetch(url,{
+        method: 'PuT',
+        headers:{
+           'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            "id" : 21,
+            "author" : "sana BENGANNOUNE",
+            "country" : "canada",
+            "imageLink" : "",
+            "language" : "arabic",
+            "link" : "",
+            "pages":"300", 
+            "title":"biography",
+            "year" :2020
+        }) 
+    })  
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+        document.getElementById("data").innerHTML= data;
+
+    })
+    
+    .catch((err)=>{
+        console.error(err)
+    })
+}
+
+//----------------- delet a book (delete)------------------------------------
+
+var delet = document.getElementById('delet');
+delet.addEventListener('click',deletBook);
+
+
+function deletBook(){
+    let url = 'http://localhost:3000/books/21'
+    fetch(url,{
+        method: 'DELETE'
+        
+    })  
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+        document.getElementById("data").innerHTML= data;
+
+    })
+    
+    .catch((err)=>{
+        console.error(err)
+    })
 }
 */
 
-
-
-
-/*var submit= document.getElementById("send")
-submit.addEventListener('click',sendBook);
-
-function sendBook(){
-
-    var toSend = {
-        id:21,
-        author:"sana"
-    };
- const jasonString = JSON.stringify(toSend);
- var xhr2= new XMLHttpRequest();
-
-
-
- xhr2.open('POST','http://localhost:3000/books');
- xhr2.setRequestHeader('Content-Type','application/json');
- xhr2.send(jasonString)
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-/*fetch("http://localhost:3000/books").then(
-    res=>{
-        res.json().then(
-            data=>{
-                console.log(data);
-                if(data.length>0){
-                    var output= "";
-
-                    // star for loop
-
-                    data.forEach((u)=>{
-                        output += `<tr>`;
-                        output += `<td>${u.id}</td>`;
-                        output += `<td>${u.author}</td>`;
-                        output += `<td>${u.country}</td>`;
-                        output += `<td>${u.imageLink}</td>`;
-                        output += `<td>${u.language}</td>`;
-                        output += `<td>${u.link}</td>`;
-                        output += `<td>${u.pages}</td>`;
-                        output += `<td>${u.title}</td>`;
-                        output += `<td>${u.year}</td></tr>`;
-                    })
-                    // close for loop
-                    document.getElementById("data").innerHTML= output;
-                }
-            }
-        )
-    }
-)
-*/
-
-
-
-/*function sendData(){
-  
-    var XHR= new XMLHttpRequest();
-    var FD= new FormData(form);
-    XHR.addEventListener("load", function(event) {
-        alert(event.target.responseText);
-      });
-    XHR.addEventListener("error", function(event) {
-        alert('Oups! Quelque chose s\'est mal passé.');
-      });
-    XHR.open("POST", "http://localhost:3000/books");
-    XHR.send(FD);
-  
-  }
-  var form = document.getElementById("form");
-  form.addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      sendData();
-    });
-    */
+// 3- methode async/await:
+//----------------- afficher all books  (get)------------------------------------
